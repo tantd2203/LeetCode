eaf58ba3785547e33b9cce89c311dfd2898ed313
tantd9 <tranduytan.220300@gmail.com>
2024-02-02 13:26:30 +0700

    1662 check if two string array are Equivalent :bus:

---
+++ src/main/java/String/_1662.java
@@ -0,0 +1,23 @@
+package String;
+
+public class _1662 {
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (String a  : word1){
+            sb1.append(a);
+        }
+        for (String a  : word2){
+            sb2.append(a);
+        }
+
+        return  sb2.toString().equals(sb1.toString());
+    }
+
+    public static void main(String[] args) {
+       String [] word1 = {"ab", "c"}, word2 = {"a", "bc"};
+        System.out.println(arrayStringsAreEqual(word1,word2));
+    }
+}
