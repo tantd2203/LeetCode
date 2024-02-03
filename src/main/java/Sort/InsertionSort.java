383170b80e04e14b0e918938d01ef9db9927e419
tantd9 <tranduytan.220300@gmail.com>
2024-02-03 10:41:25 +0700

    insert  sort :bee: :train:

---
+++ src/main/java/Sort/InsertionSort.java
@@ -0,0 +1,34 @@
+package Sort;
+
+public class InsertionSort {
+
+    private static  void Insertion_Sort(int[] array){
+
+        for (int i = 1; i < array.length ; i++) {
+            int j = i-1;
+            int ai = array[i];
+
+            while (j>=0 && ai< array[j]){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = ai;
+            printArray(i,array);
+        }
+
+
+    }
+    public static void printArray(int no , int [] a)
+    {
+        System.out.printf("%d: ", no );
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ",a[i]);
+        }
+        System.out.println( );
+    }
+    public static void main(String[] args) {
+        int a[] = {4,2,1,7,8,5};
+        Insertion_Sort(a);
+
+    }
+}
