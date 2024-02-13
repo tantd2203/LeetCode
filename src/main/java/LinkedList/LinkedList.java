cf33683df6905f7f089dc10f4a31520d0492caa7
tantd9 <tranduytan.220300@gmail.com>
2024-02-13 21:19:21 +0700

    add to index in linkedList :train:

--- src/main/java/LinkedList/LinkedList.java
+++ src/main/java/LinkedList/LinkedList.java
@@ -4,7 +4,6 @@ public class LinkedList {
     public static class Node {
         int value;
         Node next;
-
         Node(int value) {
             this.value = value;
         }
@@ -38,17 +37,56 @@ public class LinkedList {
         return newNode;
     }
 
+    public static  Node addToTail(Node headNode, int value){
+        Node  newNode =  new Node(value);
+        if (headNode == null){
+            return  newNode;
+         }
+        else{
+            Node lastNode = headNode;
+            while (lastNode.next !=null){
+                lastNode = lastNode.next;
+            }
+            lastNode.next = newNode;
+        }
+
+        return  headNode;
+    }
+
+    public static  Node addToIndex(Node headNode, int value, int index){
+        Node newNode =new Node(value);
+        if (index == 0){
+          return addToHead(headNode,value);
+        }else {
+//            step 1 Find index need add  in Node
+            Node curNode = headNode;
+            int count  =0;
+            while (curNode.next !=null){
+                count ++;
+                if (count == index){
+
+                    newNode.next = curNode.next;
+                    curNode.next = newNode;
+
+                    break;
+                }
+                curNode= curNode.next;
+            }
+        }
+
+        return headNode;
+    }
+
     public static void main(String[] args) {
         Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-
-        n1.next = n2;
-        n2.next = n3;
-
         printLinkedList(n1);
-        Node newNode = addToHead(n1,9);
+        Node newNode = addToHead(n1,0);
         printLinkedList(newNode);
+        Node addToTail = addToTail(newNode,3);
+        printLinkedList(addToTail);
+        Node addToIndex = addToIndex(addToTail,2,2);
+        printLinkedList(addToIndex);
+
 
     }
 }
