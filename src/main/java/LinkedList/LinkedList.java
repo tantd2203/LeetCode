c61febd0b03ad3ef210ef8a89613750c1faab565
tantd9 <tranduytan.220300@gmail.com>
2024-02-13 22:25:44 +0700

    fianl setting LinkedList :beer:

--- src/main/java/LinkedList/LinkedList.java
+++ src/main/java/LinkedList/LinkedList.java
@@ -1,9 +1,12 @@
 package LinkedList;
 
+import javax.swing.plaf.PanelUI;
+
 public class LinkedList {
     public static class Node {
         int value;
         Node next;
+
         Node(int value) {
             this.value = value;
         }
@@ -37,40 +40,115 @@ public class LinkedList {
         return newNode;
     }
 
-    public static  Node addToTail(Node headNode, int value){
-        Node  newNode =  new Node(value);
-        if (headNode == null){
-            return  newNode;
-         }
-        else{
+    public static Node addToTail(Node headNode, int value) {
+        Node newNode = new Node(value);
+        if (headNode == null) {
+            return newNode;
+        } else {
             Node lastNode = headNode;
-            while (lastNode.next !=null){
+            while (lastNode.next != null) {
                 lastNode = lastNode.next;
             }
             lastNode.next = newNode;
         }
 
-        return  headNode;
+        return headNode;
     }
 
-    public static  Node addToIndex(Node headNode, int value, int index){
-        Node newNode =new Node(value);
-        if (index == 0){
-          return addToHead(headNode,value);
-        }else {
+    public static Node addToIndex(Node headNode, int value, int index) {
+        Node newNode = new Node(value);
+        if (index == 0) {
+            return addToHead(headNode, value);
+        } else {
 //            step 1 Find index need add  in Node
             Node curNode = headNode;
-            int count  =0;
-            while (curNode.next !=null){
-                count ++;
-                if (count == index){
+            int count = 0;
+            while (curNode.next != null) {
+                count++;
+                if (count == index) {
 
                     newNode.next = curNode.next;
                     curNode.next = newNode;
 
                     break;
                 }
-                curNode= curNode.next;
+                curNode = curNode.next;
+            }
+        }
+
+        return headNode;
+    }
+
+    public static Node removeAtNodeHead(Node headNode) {
+
+        if (headNode != null) {
+            return headNode.next;
+        }
+        return headNode;
+    }
+
+    public static Node removeAtNodeTail(Node headNode) {
+        // Step 1 : Find last and previous
+        Node lastNode = headNode;
+        Node prevNode = null;
+        while (lastNode.next != null) {
+            prevNode = lastNode;
+            lastNode = lastNode.next;
+        }
+        if (prevNode == null) {
+            return null;
+        } else {
+            prevNode.next = lastNode.next;
+        }
+
+        return headNode;
+    }
+
+    public static int getValueNodeIndex(Node headNode, int index) {
+        Node curNode = headNode;
+
+        int count = 0;
+
+        while (curNode != null) {
+            count++;
+            curNode = curNode.next;
+            if (count == index) {
+                return curNode.value;
+            }
+        }
+        return -1;
+
+    }
+
+    public static Node removeAtIndex(Node headNode, int index) {
+
+        if (index < 0 || headNode == null) {
+            return null;
+        }
+        if (index == 0) {
+
+            return removeAtNodeHead(headNode);
+
+        }
+        Node currNode = headNode;
+        Node prevNode = null;
+        int count = 0;
+        boolean isFound = false;
+        while (currNode.next != null) {
+            if (count == index) {
+//                remove curr index
+                isFound = true;
+                break;
+            }
+            prevNode = currNode;
+            currNode = currNode.next;
+            count++;
+        }
+        if (isFound) {
+            if (prevNode == null) {  //
+                return null;
+            } else {
+                prevNode.next = currNode.next;
             }
         }
 
@@ -79,13 +157,13 @@ public class LinkedList {
 
     public static void main(String[] args) {
         Node n1 = new Node(1);
+        addToTail(n1, 2);
+        addToTail(n1, 3);
+        addToTail(n1, 4);
+
         printLinkedList(n1);
-        Node newNode = addToHead(n1,0);
-        printLinkedList(newNode);
-        Node addToTail = addToTail(newNode,3);
-        printLinkedList(addToTail);
-        Node addToIndex = addToIndex(addToTail,2,2);
-        printLinkedList(addToIndex);
+        n1 = removeAtIndex(n1, 0);
+        printLinkedList(n1);
 
 
     }
