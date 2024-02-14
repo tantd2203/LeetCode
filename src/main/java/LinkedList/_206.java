3f0431bdda9ea988a50f4f4b52d0c8b06e77af97
tantd9 <tranduytan.220300@gmail.com>
2024-02-14 22:19:39 +0700

    reverse List :bee:

---
+++ src/main/java/LinkedList/_206.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+
+
+class _206 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode reverseList(ListNode head) {
+    ListNode curNode = head;
+    while (curNode!=null && curNode.next !=null)
+    {
+        ListNode nextNode = curNode.next;
+        curNode.next = nextNode.next;
+        nextNode.next = head;
+        head = nextNode;
+    }
+    return  head;
+    }
+    public static void printLinkedList(ListNode head) {
+
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+                ListNode temp = head;
+            while (temp != null) {
+                System.out.print(temp.val);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print("->");
+                } else {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode a3 = new ListNode(3);
+        a1.next = a2; a2.next= a3;
+        printLinkedList(a1);
+        ListNode  newNode =   reverseList(a1);
+        printLinkedList(newNode);
+
+
+    }
+}
