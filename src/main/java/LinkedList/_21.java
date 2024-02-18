b3cf74be9b6e8cc0a24e4ddc6076c0180ff3137f
tantd9 <tranduytan.220300@gmail.com>
2024-02-18 21:45:57 +0700

    merge two sorted list Linkedlist :bee:

---
+++ src/main/java/LinkedList/_21.java
@@ -0,0 +1,75 @@
+package LinkedList;
+
+public class _21 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode cur = new ListNode(0);
+        ListNode temp = cur;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+                cur = cur.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+                cur = cur.next;
+            }
+        }
+        if (list1 != null) {
+            cur.next = list1;
+            list1 = list1.next;
+            cur = cur.next;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+            list2 = list2.next;
+            cur = cur.next;
+        }
+
+
+        return temp.next;
+    }
+
+    public static void printLinkedList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        _21 solution = new _21();
+        // Example lists
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode mergedList = solution.mergeTwoLists(l1, l2);
+        printLinkedList(mergedList);
+
+    }
+}
