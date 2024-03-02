49bdf1efffbbeb088caceee8db45d9148e0a6d1b
tantd9 <tranduytan.220300@gmail.com>
2024-03-02 21:50:09 +0700

    202 Happy Number :bee:

---
+++ src/main/java/HashTable/_202_Happy_Number.java
@@ -0,0 +1,34 @@
+package HashTable;
+
+import java.util.HashSet;
+
+public class _202_Happy_Number {
+
+
+    public static boolean isHappy(int n) {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        while (hashSet.add(n)) {
+            int result = 0;
+
+            while (n != 0) {
+                int digit = n % 10; // Lấy chữ số cuối cùng
+                result += digit * digit; // Tính bình phương và cộng vào tổng
+                n /= 10; // Loại bỏ chữ số cuối cùng đã được xử lý
+            }
+            if (result == 1) {
+                return true;
+            } else {
+                n = result;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+
+}
---
+++ src/main/java/String/test.java
@@ -0,0 +1,10 @@
+package String;
+
+public class test {
+    public static void main(String[] args) {
+
+        String a = "12";
+
+        System.out.println(a.charAt(0));
+    }
+}
