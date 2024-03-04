1c0ac155f4c32c568d8164af041e7ce5a95ec300
tantd9 <tranduytan.220300@gmail.com>
2024-03-04 14:32:52 +0700

    1768. Merge Strings Alternately :dog:

--- src/main/java/String/_1768_Merge_Strings_Alternately.java
+++ src/main/java/String/_1768_Merge_Strings_Alternately.java
@@ -1,2 +1,19 @@
-package String;public class _1768_Merge_Strings_Alternately {
+package String;
+
+public class _1768_Merge_Strings_Alternately {
+    public String mergeAlternately(String word1, String word2) {
+
+        StringBuilder merge = new StringBuilder();
+        int j = 0;
+        int i = 0;
+
+        while (i < word1.length() && j < word2.length()) {
+            merge.append(word1.charAt(i++));
+            merge.append(word2.charAt(j++));
+        }
+
+        merge.append(word1.substring(i));
+        merge.append(word2.substring(j));
+        return String.valueOf(merge);
+    }
 }
