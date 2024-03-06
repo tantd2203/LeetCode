8e2db15ef5ff00b85c30594d654c42f55e593e8b
tantd9 <tranduytan.220300@gmail.com>
2024-03-06 23:40:22 +0700

    151 Reverse words int a String :whale:

---
+++ src/main/java/String/_151_Reverse_Words_in_a_String.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.Arrays;
+
+public class _151_Reverse_Words_in_a_String {
+    public static String reverseWords(String s) {
+
+        String[] words = s.trim().split("\\s+");
+        // Key in Reverse Words in a String with keyword trim() remove space in head and tail of String
+        // split (\\s+)  filter space and multiple space
+
+        StringBuilder reversedSentence = new StringBuilder();
+//
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversedSentence.append(words[i]);
+            if (i != 0) {
+                reversedSentence.append(" ");
+            }
+
+        }
+        return reversedSentence.toString();
+    }
+    public static void main(String[] args) {
+        String s= "  hello world  ";
+        System.out.println(  reverseWords(s));
+    }
+}
