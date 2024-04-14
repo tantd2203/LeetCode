c73c2c65d8e40b5a1dbcaf67dad17bc894c64ec5
tantd9 <tranduytan.220300@gmail.com>
2024-04-14 22:10:35 +0700

    :banana: _2215_Find_the_Difference_of_Two_Arrays

--- src/main/java/HashTable/_2215_Find_the_Difference_of_Two_Arrays.java
+++ src/main/java/HashTable/_2215_Find_the_Difference_of_Two_Arrays.java
@@ -6,24 +6,47 @@ import java.util.List;
 
 public class _2215_Find_the_Difference_of_Two_Arrays {
 
-    public List<Integer> findDifference(int[] nums1, int[] nums2) {
-
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         HashSet<Integer> hashSet1 = new HashSet<>();
         HashSet<Integer> hashSet2 = new HashSet<>();
-
         List<List<Integer>> result = new ArrayList<>();
-        for (Integer i : nums1) {
-            hashSet1.add(i);
+
+        for (int num : nums1) {
+            hashSet1.add(num);
         }
-        for (Integer i : nums2) {
-            hashSet2.add(i);
+        for (int num : nums2) {
+            hashSet2.add(num);
         }
 
-        hashSet1.removeAll(hashSet2);
-        return new ArrayList<>(hashSet1);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        // Find elements in nums1 but not in nums2
+        for (int num : hashSet1) {
+            if (!hashSet2.contains(num)) {
+                list1.add(num);
+            }
+        }
+        result.add(list1);
+
+        // Find elements in nums2 but not in nums1
+        for (int num : hashSet2) {
+            if (!hashSet1.contains(num)) {
+                list2.add(num);
+            }
+        }
+        result.add(list2);
+
+        return result;
     }
 
     public static void main(String[] args) {
-
+        _2215_Find_the_Difference_of_Two_Arrays ftd = new _2215_Find_the_Difference_of_Two_Arrays();
+        int[] nums1 = {-1000};
+        int[] nums2 = {-1000};
+        List<List<Integer>> result = ftd.findDifference(nums1, nums2);
+        for (List<Integer> list : result) {
+            System.out.println(list);
+        }
     }
 }
---
+++ src/main/java/String/_67_Add_Binary.java
@@ -0,0 +1,10 @@
+package String;
+
+public class _67_Add_Binary {
+
+    public String addBinary(String a, String b) {
+
+
+        return "";
+    }
+}
