190227a3bfde0f5426ea73de0742c49b1cd6a137
tantd9 <tranduytan.220300@gmail.com>
2024-04-23 15:38:48 +0700

    solution for problem :banana:

--- src/main/java/HashTable/_2352_Equal_Row_and_Column_Pairs.java
+++ src/main/java/HashTable/_2352_Equal_Row_and_Column_Pairs.java
@@ -7,22 +7,26 @@ import java.util.Map;
 
 public class _2352_Equal_Row_and_Column_Pairs {
     public static int equalPairs(int[][] grid) {
-        int count = 0;
         int n = grid.length;
+        int count = 0;
 
-        Map<String, Integer> rowCounter = new HashMap<>();
+        HashMap<String, Integer> rowMap = new HashMap<>();
 
         for (int[] row : grid) {
 
             String rowString = Arrays.toString(row);
-            rowCounter.put(rowString, rowCounter.getOrDefault(rowString, 0) + 1);
+
+            rowMap.put(rowString, rowMap.getOrDefault(rowString, 0) + 1);
         }
-        for (int c = 0; c < n; c++) {
+
+
+        for (int i = 0; i < n; i++) {
             int[] colArray = new int[n];
-            for (int r = 0; r < n; r++) {
-                colArray[r] = grid[r][c];
+            for (int j = 0; j < n; j++) {
+                colArray[j] = grid[j][i];
             }
-            count += rowCounter.getOrDefault(Arrays.toString(colArray), 0);
+
+            count += rowMap.getOrDefault(Arrays.toString(colArray), 0);
         }
 
         return count;
@@ -30,6 +34,8 @@ public class _2352_Equal_Row_and_Column_Pairs {
 
     public static void main(String[] args) {
         int[][] grid = {{3, 2, 1}, {1, 7, 6}, {2, 7, 7}};
-        System.out.println(        equalPairs(grid));
+        System.out.println(equalPairs(grid));
     }
 }
+// idea bài này là put hết row vô cái row list và cho cái giá trị của nó = 1 , nếu chưa cho cái row đó trong map =1 có r  = 0
+// xong đến đoạn đếm số column thì xem thử đã có chưa nếu có r thì +1 đơn vị nếu chưa thì k
