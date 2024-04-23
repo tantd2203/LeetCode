b825adad90fbac3c66d1f2e8ccec5f04f8a57ede
tantd9 <tranduytan.220300@gmail.com>
2024-04-23 11:43:10 +0700

    _2352_Equal_Row_and_Column_Pairs :banana:

--- src/main/java/HashTable/_2352_Equal_Row_and_Column_Pairs.java
+++ src/main/java/HashTable/_2352_Equal_Row_and_Column_Pairs.java
@@ -1,27 +1,35 @@
 package HashTable;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class _2352_Equal_Row_and_Column_Pairs {
     public static int equalPairs(int[][] grid) {
-        int row = grid.length;
-        int column = grid[0].length;
         int count = 0;
-        HashSet<Integer> rowHashSet = new HashSet<>();
-        HashSet<Integer> columnHastSet = new HashSet<>();
+        int n = grid.length;
 
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < column; j++) {
-                rowHashSet.add(grid[i][j]);
-                System.out.println();
-            }
-            System.out.println();
+        Map<String, Integer> rowCounter = new HashMap<>();
+
+        for (int[] row : grid) {
+
+            String rowString = Arrays.toString(row);
+            rowCounter.put(rowString, rowCounter.getOrDefault(rowString, 0) + 1);
         }
+        for (int c = 0; c < n; c++) {
+            int[] colArray = new int[n];
+            for (int r = 0; r < n; r++) {
+                colArray[r] = grid[r][c];
+            }
+            count += rowCounter.getOrDefault(Arrays.toString(colArray), 0);
+        }
+
         return count;
     }
 
     public static void main(String[] args) {
         int[][] grid = {{3, 2, 1}, {1, 7, 6}, {2, 7, 7}};
-        equalPairs(grid);
+        System.out.println(        equalPairs(grid));
     }
 }
