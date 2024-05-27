c8b71ac08c6e67d41567a8f9506f90e643642b0e
tantd9 <tranduytan.220300@gmail.com>
2024-05-27 16:37:18 +0700

    _701_Insert_into_a_Binary_Search_Tree

--- src/main/java/BinaryTreeDFS/_104_Maximum_Depth_of_Binary_Tree.java
+++ src/main/java/BinaryTreeDFS/_104_Maximum_Depth_of_Binary_Tree.java
@@ -1,24 +1,5 @@
 package BinaryTreeDFS;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class _104_Maximum_Depth_of_Binary_Tree {
     public int maxDepth(TreeNode root) {
 
---
+++ src/main/java/BinaryTreeDFS/_701_Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,56 @@
+package BinaryTreeDFS;
+
+
+public class MyBST {
+    private TreeNode mRoot;
+
+    public TreeNode insert(TreeNode root, int val) {
+        TreeNode newNode = new TreeNode(val);
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        } else {
+            TreeNode temp = root;
+            while (true) {
+                if (val > temp.val) {
+                    if (temp.right == null) {
+                        temp.right = newNode;
+                        break;
+                    } else {
+                        temp = temp.right;
+                    }
+                } else {
+                    if (temp.left == null) {
+                        temp.left = newNode;
+                        break;
+                    } else {
+                        temp = temp.left;
+                    }
+                }
+            }
+            return root;
+        }
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyBST bst = new MyBST();
+        bst.mRoot = bst.insert(bst.mRoot, 5);
+        bst.mRoot = bst.insert(bst.mRoot, 3);
+        bst.mRoot = bst.insert(bst.mRoot, 7);
+        bst.mRoot = bst.insert(bst.mRoot, 2);
+        bst.mRoot = bst.insert(bst.mRoot, 4);
+        bst.mRoot = bst.insert(bst.mRoot, 6);
+        bst.mRoot = bst.insert(bst.mRoot, 8);
+
+        System.out.println("In-order traversal of the BST:");
+        bst.inOrderTraversal(bst.mRoot);  // Expected output: 2 3 4 5 6 7 8
+    }
+}
