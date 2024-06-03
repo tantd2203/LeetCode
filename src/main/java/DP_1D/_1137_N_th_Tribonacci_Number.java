b565e0a46e57c937a49aff7741dacf4833c2ba5e
tantd9 <tranduytan.220300@gmail.com>
2024-06-03 21:02:17 +0700

    _1137_N_th_Tribonacci_Number

---
+++ src/main/java/DP_1D/_1137_N_th_Tribonacci_Number.java
@@ -0,0 +1,30 @@
+package DP_1D;
+
+public class _1137_N_th_Tribonacci_Number {
+    public int tribonacci(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        // Initialize the first three Tribonacci numbers
+        int t0 = 0, t1 = 1, t2 = 1;
+
+        // Variable to store the next Tribonacci number
+        int tn = 0;
+
+        // Iterate to compute the n-th Tribonacci number
+        for (int i = 3; i <= n; i++) {
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+
+        return t2;
+
+    }
+}
