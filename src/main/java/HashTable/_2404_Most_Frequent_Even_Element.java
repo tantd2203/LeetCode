7686b90abb391c70f2b2fc2fe47341c3244b2b6e
tantd9 <tranduytan.220300@gmail.com>
2024-06-23 22:43:10 +0700

    _2404_Most_Frequent_Even_Element

---
+++ src/main/java/HashTable/_2404_Most_Frequent_Even_Element.java
@@ -0,0 +1,37 @@
+package HashTable;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class _2404_Most_Frequent_Even_Element {
+    public static int mostFrequentEven(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Arrays.sort(nums);
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        int elementMax = 0;
+        int index = 0;
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > elementMax) {
+                elementMax = map.get(key);
+                index = key;
+            }
+        }
+        if (map.isEmpty()) return -1;
+        if (elementMax == 0) return -1;
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {8267, 8727, 2649, 7359, 2142, 7824, 7399, 3424, 8868, 476, 9769, 4631, 6631, 4205, 9178, 7920, 4748, 5523, 74, 5550, 516, 6448, 3475, 7867, 7041, 5896, 4382, 2066, 8101, 674, 67, 9311, 4308, 1693, 4586, 3959, 2973, 2158, 5067, 9060, 2988, 1147, 1746, 9722, 8550, 9126, 3029, 1677, 4154, 5473, 6589, 5381, 1486, 8699, 792, 8349, 6773, 496, 4505, 6163, 6843, 1232, 4809, 4432, 6251, 7658, 4949, 1419, 7005, 3815, 6295, 4635, 7785, 4891, 6941, 6952, 7218, 134, 2718, 7409, 9551};
+        System.out.println(mostFrequentEven(nums)
+        );
+    }
+
+}
---
+++ src/main/java/String/_409_Longest_Palindrome.java
@@ -0,0 +1,4 @@
+package String;
+
+public class _409_Longest_Palindrome {
+}
